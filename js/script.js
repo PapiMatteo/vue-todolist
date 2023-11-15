eae953cbbea4dba1530fb7383e62e7f7997f566f
PapiMatteo <matteopapi.work@gmail.com>
2023-11-15 15:19:19 +0100

    add: adding item to todoList array by click on button or key press Enter and remove by X click

--- js/script.js
+++ js/script.js
@@ -3,8 +3,17 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-
+            newTodo: "",
+            todoList: []
         }
     },
-    methods
+    methods: {
+        addTodo: function() {
+            this.todoList.push(this.newTodo);
+            this.newTodo = "";
+        },
+        removeTodo: function(todoIndex) {
+            this.todoList.splice(todoIndex, 1)
+        }
+    }
 }).mount("#app");
