764febea1a9e8d792a37ffbfd36168fe9a2e06d3
PapiMatteo <matteopapi.work@gmail.com>
2023-11-15 16:04:52 +0100

    add: js correction

--- js/script.js
+++ js/script.js
@@ -8,18 +8,25 @@ createApp({
                 done: false
             },
             todoList: []
-        }
+        };
     },
     methods: {
         addTodo: function() {
+
             this.todoList.push({...this.newTodo});
             this.newTodo.text = "";
+
         },
         removeTodo: function(todoIndex) {
-            this.todoList.splice(todoIndex, 1)
+
+            this.todoList.splice(todoIndex, 1);
+
         },
         setDone: function(todoIndex) {
+
             this.todoList[todoIndex].done = !this.todoList[todoIndex].done;
+
         }
+
     }
 }).mount("#app");
